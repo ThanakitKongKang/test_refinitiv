827d4f46d9f4f4f4ea5f92a9bfc2045799b59330
thanakit.c <thanakit@kkumail.com>
2021-03-18 21:44:47 +0700

    complete q2

--- section2-q1-q2/src/app/q2/q2.component.ts
+++ section2-q1-q2/src/app/q2/q2.component.ts
@@ -1,21 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-q2',
   templateUrl: './q2.component.html',
   styleUrls: ['./q2.component.scss']
 })
-export class Q2Component implements OnInit {
 
-  constructor(private httpClient: HttpClient) { }
+export class Q2Component implements OnInit {
+  url = 'https://api.publicapis.org/categories'
+  categories: string[] = [];
+  filteredCategories: string[] = []
+  input: string = '';
+  isLoading: boolean = true;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     console.log(this.getTableData())
   }
 
-  getTableData(): Observable<Object> {
-    return this.httpClient.get('https://api.publicapis.org/categories');
+  getTableData(): void {
+    this.http.get<string[]>(this.url)
+      .pipe(tap(_ => console.log('fetch categories')))
+      .subscribe(res => {
+        this.categories = res;
+        this.filter();
+      })
+  }
+
+  filter(): void {
+    this.isLoading = true;
+    this.filteredCategories = this.categories.filter(category => category.toLowerCase().includes(this.input.trim().toLowerCase()))
+    this.isLoading = false;
+
   }
 }
